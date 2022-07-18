1cd565a87eecdd70337bd87adbb876019e44d251
Crush12999 <1299975117@qq.com>
2022-07-18 20:07:15 +0800

    feat: 实现 SpringBoot Admin 监控服务告警通知
    1、实现 SpringBoot Admin 监控服务告警通知

---
+++ e-commerce-admin/src/main/java/com/sryzzz/ecommerce/notifier/MyNotifier.java
@@ -0,0 +1,45 @@
+package com.sryzzz.ecommerce.notifier;
+
+import de.codecentric.boot.admin.server.domain.entities.Instance;
+import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
+import de.codecentric.boot.admin.server.domain.events.InstanceEvent;
+import de.codecentric.boot.admin.server.domain.events.InstanceStatusChangedEvent;
+import de.codecentric.boot.admin.server.notify.AbstractEventNotifier;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author sryzzz
+ * @create 2022/7/18 20:00
+ * @description 自定义告警
+ */
+@Slf4j
+@Component
+@SuppressWarnings("all")
+public class MyNotifier extends AbstractEventNotifier {
+
+    protected MyNotifier(InstanceRepository repository) {
+        super(repository);
+    }
+
+    /**
+     * 实现对事件的通知
+     */
+    @Override
+    protected Mono<Void> doNotify(InstanceEvent event, Instance instance) {
+        return Mono.fromRunnable(() -> {
+
+            if (event instanceof InstanceStatusChangedEvent) {
+                log.info("Instance Status Change: [{}], [{}], [{}]",
+                        instance.getRegistration().getName(), event.getInstance(),
+                        ((InstanceStatusChangedEvent) event).getStatusInfo().getStatus());
+            } else {
+                log.info("Instance Info: [{}], [{}], [{}]",
+                        instance.getRegistration().getName(), event.getInstance(),
+                        event.getType());
+            }
+
+        });
+    }
+}
