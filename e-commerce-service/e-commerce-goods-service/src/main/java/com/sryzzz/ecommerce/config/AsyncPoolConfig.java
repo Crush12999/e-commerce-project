b747f14a570a91f522df6c8cd3798cc1ed975271
Crush12999 <1299975117@qq.com>
2022-07-25 23:07:22 +0800

    feat: 异步入库商品服务功能实现
    1、异步入库商品服务功能实现

---
+++ e-commerce-service/e-commerce-goods-service/src/main/java/com/sryzzz/ecommerce/config/AsyncPoolConfig.java
@@ -0,0 +1,82 @@
+package com.sryzzz.ecommerce.config;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author sryzzz
+ * @create 2022/7/25 22:41
+ * @description 自定义异步任务线程池，异步任务异常捕获处理器
+ * 开启 Spring 异步支持：@EnableAsync
+ */
+@Slf4j
+@EnableAsync
+@Configuration
+public class AsyncPoolConfig implements AsyncConfigurer {
+
+    /**
+     * 将自定义的线程池注入到 Spring 容器中
+     */
+    @Bean
+    @Override
+    public Executor getAsyncExecutor() {
+
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(20);
+        executor.setKeepAliveSeconds(60);
+        // 这个非常重要
+        executor.setThreadNamePrefix("Ecommerce-Async-");
+
+        // 等待所有任务结果候再关闭线程池
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(60);
+        // 定义拒绝策略
+        executor.setRejectedExecutionHandler(
+                new ThreadPoolExecutor.CallerRunsPolicy()
+        );
+        // 初始化线程池, 初始化 core 线程
+        executor.initialize();
+
+        return executor;
+    }
+
+    /**
+     * 指定系统中的异步任务在出现异常时使用到的处理器
+     */
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new AsyncExceptionHandler();
+    }
+
+    /**
+     * 异步任务异常捕获处理器
+     */
+    @SuppressWarnings("all")
+    class AsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+
+        @Override
+        public void handleUncaughtException(Throwable throwable, Method method,
+                                            Object... objects) {
+
+            throwable.printStackTrace();
+            log.error("Async Error: [{}], Method: [{}], Param: [{}]",
+                    throwable.getMessage(), method.getName(),
+                    JSON.toJSONString(objects));
+
+            // TODO 发送邮件或者是短信, 做进一步的报警处理
+        }
+    }
+}
---
+++ e-commerce-service/e-commerce-goods-service/src/main/java/com/sryzzz/ecommerce/constant/GoodsConstant.java
@@ -0,0 +1,14 @@
+package com.sryzzz.ecommerce.constant;
+
+/**
+ * @author sryzzz
+ * @create 2022/7/25 23:02
+ * @description 商品常量信息
+ */
+public class GoodsConstant {
+
+    /**
+     * redis key
+     */
+    public static final String ECOMMERCE_GOODS_DICT_KEY = "ecommerce:goods:dict:20220101";
+}
