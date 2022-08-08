d73b8099b9a6c348d600e5c38ff74ace35ddb7f8
Crush12999 <1299975117@qq.com>
2022-08-08 23:23:08 +0800

    test: 学习使用编程方式开启 Hystrix 请求缓存

--- e-commerce-alibaba-nacos-client/src/main/java/com/sryzzz/ecommerce/NacosClientApplication.java
+++ e-commerce-alibaba-nacos-client/src/main/java/com/sryzzz/ecommerce/NacosClientApplication.java
@@ -2,6 +2,7 @@ package com.sryzzz.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @description Nacos Client 工程启动入口
  * "@RefreshScope" 刷新配置
  */
+@ServletComponentScan
 @EnableCircuitBreaker
 @EnableFeignClients
 @RefreshScope
--- e-commerce-alibaba-nacos-client/src/main/java/com/sryzzz/ecommerce/controller/HystrixController.java
+++ e-commerce-alibaba-nacos-client/src/main/java/com/sryzzz/ecommerce/controller/HystrixController.java
@@ -2,6 +2,7 @@ package com.sryzzz.ecommerce.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.sryzzz.ecommerce.service.NacosClientService;
+import com.sryzzz.ecommerce.service.hystrix.CacheHystrixCommand;
 import com.sryzzz.ecommerce.service.hystrix.NacosClientHystrixCommand;
 import com.sryzzz.ecommerce.service.hystrix.NacosClientHystrixObservableCommand;
 import com.sryzzz.ecommerce.service.hystrix.UseHystrixCommandAnnotation;
@@ -123,6 +124,39 @@ public class HystrixController {
         log.info("observable command result is : [{}], [{}]",
                 JSON.toJSONString(result), Thread.currentThread().getName());
         return result.get(0);
+    }
+
+    @GetMapping("/cache-hystrix-command")
+    public void cacheHystrixCommand(@RequestParam String serviceId) {
+
+        // 使用缓存 Command, 发起两次请求
+        CacheHystrixCommand command1 = new CacheHystrixCommand(
+                nacosClientService, serviceId
+        );
+        CacheHystrixCommand command2 = new CacheHystrixCommand(
+                nacosClientService, serviceId
+        );
+
+        List<ServiceInstance> result01 = command1.execute();
+        List<ServiceInstance> result02 = command2.execute();
+        log.info("result01, result02: [{}], [{}]",
+                JSON.toJSONString(result01), JSON.toJSONString(result02));
+
+        // 清除缓存
+        CacheHystrixCommand.flushRequestCache(serviceId);
+
+        // 使用缓存 Command, 发起两次请求
+        CacheHystrixCommand command3 = new CacheHystrixCommand(
+                nacosClientService, serviceId
+        );
+        CacheHystrixCommand command4 = new CacheHystrixCommand(
+                nacosClientService, serviceId
+        );
+
+        List<ServiceInstance> result03 = command3.execute();
+        List<ServiceInstance> result04 = command4.execute();
+        log.info("result03, result04: [{}], [{}]",
+                JSON.toJSONString(result03), JSON.toJSONString(result04));
     }
 
 }
